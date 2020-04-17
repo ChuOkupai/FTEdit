37a4b556c7e5e4366ea39230f5845538f8f6cce3
Andrewraks <43434639+Andrewraks@users.noreply.github.com>
2020-04-17 17:40:54 +0200

    Update Node.cc

--- src/editor/Node.cc
+++ src/editor/Node.cc
@@ -1,1 +1,26 @@
 #include "Node.hh"
+
+
+Node::Node(QPoint position){
+    this->position = position; 
+}
+
+Gate* Node::getParent(){
+  return this->parent;
+}
+
+Qpoint Node::getPosition(){
+  return this->position; 
+}
+
+void Node::setPostion(Qpoint position){
+    this->position = position; 
+}
+
+void Node::attach(Gate* parent){
+    this->parent = parent; 
+}
+
+Node* Node::detach(){
+  
+}
