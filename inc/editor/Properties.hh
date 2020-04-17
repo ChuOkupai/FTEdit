2e1606de2008ff247881637537d110051cb11e4d
mouridoullah <55403324+mouridoullah@users.noreply.github.com>
2020-04-17 17:20:27 +0200

    Update Properties.hh

--- inc/editor/Properties.hh
+++ inc/editor/Properties.hh
@@ -1,1 +1,31 @@
 #pragma once
+#ifndef PROPERTIES_H
+#define PROPERTIES_H 
+
+#include <iostream>
+
+class Properties
+{
+	public:
+		Properties(QString name);
+		~Properties();
+
+		QString const getName();
+		QString const getDesc();
+		bool const getKeep();
+
+		void setName(QString name);
+		void setDesc(QString desc);
+		void setKeep(bool keep);
+
+		void decrementRefCount();
+		void incrementRefCount();
+		bool canBeRemoved();
+
+	private:
+		QString name;
+		QString desc;
+		int refCount;
+		bool keep;
+};
+#endif
