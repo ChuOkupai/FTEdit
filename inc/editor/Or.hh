636902566892c9882c1ed19cdd7cd39e5601473c
nazberrypie <franck.naze78@gmail.com>
2020-04-17 18:31:36 +0200

    update Or.hh

--- inc/editor/Or.hh
+++ inc/editor/Or.hh
@@ -1,1 +1,14 @@
 #pragma once
+#include <QString>
+#include <QList>
+
+class Or : public Gate
+{
+private:
+    Or(QString name);
+    ~Or();
+    double getProbability(double time);
+    bool check (QList<QString>& errors);
+    void accept(Editor& editor,EditVisitor& visitor);
+    void accept(RenderVisitor& visitor);
+};
