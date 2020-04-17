8c52b73d97419e78b26c4f294689d33d687eeb23
nazberrypie <franck.naze78@gmail.com>
2020-04-17 18:38:32 +0200

    Update And Or .hh

--- inc/editor/And.hh
+++ inc/editor/And.hh
@@ -1,1 +1,16 @@
 #pragma once
+#include <QList>
+#include <QString>
+
+
+class And : public Gate
+{
+private:
+    And(QString name);
+    ~And();
+    double getProbability(double time);
+    bool check (QList<QString>& errors);
+    void accept(Editor& editor,EditVisitor& visitor);
+    void accept(RenderVisitor& visitor);
+
+};
