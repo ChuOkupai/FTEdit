8c65fa105960688fa8ab80f003fa1f416a454c61
Xunyue Hu <xunyue.hu@ens.uvsq.fr>
2020-04-28 13:09:28 +0200

    Evaluator.cc
    pas de grande chose, et j’ai utilisé creator du coup y'a Makefile.pro.user

--- src/evaluator/Evaluator.cc
+++ src/evaluator/Evaluator.cc
@@ -3,3 +3,18 @@
 
 
 
+Evaluator::Evaluator(Gate* top,double missionTime, double step)
+{
+    this->top = top;
+    this->missionTime = missionTime;
+    this->step = step;
+}
+Evaluator::~Evaluator(){}
+double Evaluator::getMissionTime()
+{
+    return missionTime;
+}
+double Evaluator::getStep()
+{
+    return step;
+}
