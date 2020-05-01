3f77e0768441b271412242da9c9c988f3bc62c12
Andrewraks <andrewraks@gmail.com>
2020-05-01 20:37:53 +0200

    fix error

--- src/evaluator/EvalVisitor.cc
+++ src/evaluator/EvalVisitor.cc
@@ -9,41 +9,40 @@ inline bool instanceof(const T*) {
 }
 
 EvalVisitor::EvalVisitor(){}
-/*
+
 void EvalVisitor::visit(And& andVisitor){
-    for(int i = 0; i < andVisitor.getChildren().size() ; i++){
-        if( instanceof<Gate>(andVisitor.getChildren().at(i)) ){
-            this->visit(andVisitor.getChildren().at(i));
-        }else if(instanceof<Container>(andVisitor.getChildren().at(i))){
-            andVisitor.getChildren().at(i)->getProbability(0); //TODO
+    for(int i = 0; i < andVisitor.getChildren()->size() ; i++){
+        if( instanceof<Gate>(andVisitor.getChildren()->at(i)) ){
+            visit(andVisitor.getChildren()->at(i));
+        }else if(instanceof<Container>(andVisitor.getChildren()->at(i))){
+            andVisitor.getChildren()->at(i)->getProbability(0); //TODO
         }else{
-            this->visit(andVisitor.getChildren().at(i));
+            visit(andVisitor.getChildren()->at(i));
         }
     }
 }
 
 void EvalVisitor::visit(Or&  orVisitor){
-    for(int i = 0; i < orVisitor.getChildren().size() ; i++){
-        if( instanceof<Gate>(orVisitor.getChildren().at(i)) ){
-            this->visit(orVisitor.getChildren().at(i));
-        }else if(instanceof<Container>(orVisitor.getChildren().at(i))){
-            orVisitor.getChildren().at(i)->getProbability(0); //TODO
+    for(int i = 0; i < orVisitor.getChildren()->size() ; i++){
+        if( instanceof<Gate>(orVisitor.getChildren()->at(i)) ){
+            visit(orVisitor.getChildren()->at(i));
+        }else if(instanceof<Container>(orVisitor.getChildren()->at(i))){
+            orVisitor.getChildren()->at(i)->getProbability(0); //TODO
         }else{
-            this->visit(orVisitor.getChildren().at(i));
+            visit(orVisitor.getChildren()->at(i));
         }
     }
 }
 
-void EvalVisitor::visit(Inhibit& inVisitor){
+/*void EvalVisitor::visit(Inhibit& inVisitor){
 
 }
 
-void visit(Transfert& transVisitor){
+void EvalVisitor::visit(Transfert& transVisitor){
     
 }
-
+*/
 void visit(Container& eventVisitor){
     eventVisitor.getProbability(0);
 
 }
-*/
