76fd2870fedec087af3bd6bc82e910b3fc52f181
Xunyue Hu <xunyue.hu@ens.uvsq.fr>
2020-05-01 18:36:46 +0200

    sieveOfAtkin

--- src/evaluator/ResultMCS.cc
+++ src/evaluator/ResultMCS.cc
@@ -27,24 +27,24 @@ ResultMCS::~ResultMCS(){}
 //        for(int j=0; j<cs[i].size();j++){
 
 //            if(instanceof<Gate>(cs[i][j])){/*si c'est une Porte*/
-//                QList<Node*>& childrens = cs[i][j]->getChildren();/*recuperer les fils de cette porte*/
+//                QList<Node*>* childrens = cs[i][j]->getChildren();/*recuperer les fils de cette porte*/
 
 //                if(instanceof<And>(cs[i][j])){/*si c'est une Porte AND*/
-//                    cs[i][j] = childrens[0];
-//                    for(int m=1; m<childrens.size();m++){
+//                    cs[i][j] = childrens->at(0);
+//                    for(int m=1; m<childrens->size();m++){
 //                        cs[i].append(childrens[m]);
 //                    }
 //                }
 //                else if(instanceof<Or>(cs[i][j])){/*si c'est une Porte OR*/
-//                    cs[i][j] = childrens[0];/*remplacer cette porte de son premier fils*/
-//                    for(int m=1; m<childrens.size();m++){/*parcourir les enfants*/
+//                    cs[i][j] = childrens->at(0);/*remplacer cette porte de son premier fils*/
+//                    for(int m=1; m<childrens->size();m++){/*parcourir les enfants*/
 //                        lstcopy = new QList<Node*>;/*faire le pointer pointe une autre nouvelle liste vide*/
 
 //                        for(int n=0; n<cs[i].size();n++){/*copier une nouvelle liste avec le meme contenu*/
 //                            lstcopy->append(cs[i][n]);
 //                        }
 
-//                        lstcopy->replace(j,childrens[m]);/*mettre les autres fils dans la meme position de leurs liste*/
+//                        lstcopy->replace(j,childrens->at(m));/*mettre les autres fils dans la meme position de leurs liste*/
 //                        cs.append(*lstcopy); /*mettre cette nouvelle liste a la fin de cs*/
 
 //                    }
@@ -61,7 +61,7 @@ ResultMCS::~ResultMCS(){}
 //    for(int i=0; i<cs.size(); i++){
 //        mcs.append(QList<Event>());
 //        for(int j=0; j<cs[i].size();j++){
-//            mcs[i].append(cs[i][j]->getEvent());
+//            mcs[i].append(*cs[i][j]->getEvent());
 //        }
 //    }
 //}
@@ -117,7 +117,7 @@ ResultMCS::~ResultMCS(){}
 //}
 
 
-///*Trie les coupes en triant par ordre croissant selon l'assignation des nombres premiers */
+/*Trie les coupes en triant par ordre croissant selon l'assignation des nombres premiers */
 //void ResultMCS::sortCut(QMap<QList<Event>, int> mapMul){
 //    QList<Event> temp;
 
@@ -135,59 +135,59 @@ ResultMCS::~ResultMCS(){}
 
 //}
 
-//QList<int> ResultMCS::sieveOfAtkin(int limit){/*Génère un nombre de nombres premiers selon le nombre limite n.*/
-//    QList<int> premiers;
-//    bool sieve_list[limit + 1];
-//    int n, x, y, k, i;
+QList<int> ResultMCS::sieveOfAtkin(int limit){/*Génère un nombre de nombres premiers selon le nombre limite n.*/
+    QList<int> premiers;
+    bool sieve_list[limit + 1];
+    int n, x, y, k, i;
 
-//    /* is_prime(i) ← false, ∀ i ∈ [5, limit] */
-//    for(n = 5; n <= limit; n++){
-//        sieve_list[n] = false;
-//    }
+    /* is_prime(i) ← false, ∀ i ∈ [5, limit] */
+    for(n = 5; n <= limit; n++){
+        sieve_list[n] = false;
+    }
 
-//    /* for (x, y) in [1, √limit] × [1, √limit]: */
-//    for(x = 1; x <= (int)sqrt(limit); x++){
-//        for(y = 1; y <= (int)sqrt(limit); y++){
-//            /*n ← 4x²+y²:if (n ≤ limit) and (n mod 12 = 1 or n mod 12 = 5):is_prime(n) ← ¬is_prime(n)*/
-//            n = 4 * x * x + y * y;
-//            if(n <= limit && (n % 12 == 1 || n % 12 == 5))
-//                sieve_list[n] = ~sieve_list[n];
-//            /*n ← 3x²+y²:if (n ≤ limit) and (n mod 12 = 7):is_prime(n) ← ¬is_prime(n)*/
-//            n = 3 * x * x + y * y;
-//            if(n <= limit && n % 12 == 7)
-//                sieve_list[n] = ~sieve_list[n];
-//            /*n ← 3x²-y²:if (x > y) and (n ≤ limit) and (n mod 12 = 11):is_prime(n) ← ¬is_prime(n)*/
-//            n = 3 * x * x - y * y;
-//            if(x > y && n <= limit && n % 12 == 11)
-//                sieve_list[n] = ~sieve_list[n];
-//        }
-//    }
+    /* for (x, y) in [1, √limit] × [1, √limit]: */
+    for(x = 1; x <= (int)sqrt(limit); x++){
+        for(y = 1; y <= (int)sqrt(limit); y++){
+            /*n ← 4x²+y²:if (n ≤ limit) and (n mod 12 = 1 or n mod 12 = 5):is_prime(n) ← ¬is_prime(n)*/
+            n = 4 * x * x + y * y;
+            if(n <= limit && (n % 12 == 1 || n % 12 == 5))
+                sieve_list[n] = ~sieve_list[n];
+            /*n ← 3x²+y²:if (n ≤ limit) and (n mod 12 = 7):is_prime(n) ← ¬is_prime(n)*/
+            n = 3 * x * x + y * y;
+            if(n <= limit && n % 12 == 7)
+                sieve_list[n] = ~sieve_list[n];
+            /*n ← 3x²-y²:if (x > y) and (n ≤ limit) and (n mod 12 = 11):is_prime(n) ← ¬is_prime(n)*/
+            n = 3 * x * x - y * y;
+            if(x > y && n <= limit && n % 12 == 11)
+                sieve_list[n] = ~sieve_list[n];
+        }
+    }
 
-//    /*for n in [5, √limit]: if is_prime(n):is_prime(k) ← false, k ∈ {n², 2n², 3n², ..., limit}*/
-//    for(n = 5; n <= sqrt(limit); n++){
-//        if(sieve_list[n] == true){
-//            i = 1;
-//            k = i++ * n * n;
-//            while(k <= limit){
-//                sieve_list[k] = false;
-//                k = i++ * n * n;
-//            }
-//        }
-//    }
-//    premiers<<2<<3;/* append 2, 3 */
-//    for(n = 5; n <= limit; n++){ /* append les premiers suivants*/
-//        if(sieve_list[n] == true)
-//            premiers<<n;
-//    }
+    /*for n in [5, √limit]: if is_prime(n):is_prime(k) ← false, k ∈ {n², 2n², 3n², ..., limit}*/
+    for(n = 5; n <= sqrt(limit); n++){
+        if(sieve_list[n] == true){
+            i = 1;
+            k = i++ * n * n;
+            while(k <= limit){
+                sieve_list[k] = false;
+                k = i++ * n * n;
+            }
+        }
+    }
+    premiers<<2<<3;/* append 2, 3 */
+    for(n = 5; n <= limit; n++){ /* append les premiers suivants*/
+        if(sieve_list[n] == true)
+            premiers<<n;
+    }
 
-//    return premiers;
-//}
+    return premiers;
+}
 
 
 QList<QList<Event>> ResultMCS::getMCS(){
-    computeCS();
-    convertCS();
-    reduceCS();
+//    computeCS();
+//    convertCS();
+//    reduceCS();
     return mcs;
 }
 QList<double> ResultMCS::getProbabilities(){
