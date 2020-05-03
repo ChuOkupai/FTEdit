0bf37247ca9cbd651b9cc7060dc6204685be8145
nazberrypie <francknaze78@gmail.com>
2020-05-03 17:44:07 +0200

    Modified EvalVisitor to remove compiling issues, deleted useless headers

--- src/evaluator/EvalVisitor.cc
+++ src/evaluator/EvalVisitor.cc
@@ -7,6 +7,8 @@
  *
  * ***/
 
+//les méthodes visit() ne sont plus commentés. Elles furent légèrement modifiés pour pouvoir compiler.  
+
 using namespace std;
 
 template<typename Base, typename T>
@@ -30,49 +32,50 @@ void EvalVisitor::setTime(double time){
 void EvalVisitor::reset(){
 	this->proba = 0;
 }
-/*
+
 double EvalVisitor::visit(And& andVisitor){
 
 
 	for(int i = 1; i < andVisitor.getChildren().size() ; i++){
 		if(this->proba ==0){
-			//return andVisitor.getChildren().at(i)->accept(this);
+			return andVisitor.getChildren().at(i)->accept(*this);
 
 		}else{
-		// return this->proba * andVisitor.getChildren().at(i)->accept(this);
+		 return this->proba * andVisitor.getChildren().at(i)->accept(*this);
 		}
 	}
+	return 0;//to remove compiling warning
 }
 
 double EvalVisitor::visit(Or&  orVisitor){
 
 	for(int i = 0; i < orVisitor.getChildren().size() ; i++){
 
-			//return this->proba + orVisitor.getChildren().at(i)->accept(this);
+			return this->proba + orVisitor.getChildren().at(i)->accept(*this);
 		}
-
+	return 0;//to remove compiling warning
 }
 
 double EvalVisitor::visit(Inhibit& inVisitor){
 	inVisitor.getParent();
 	if(inVisitor.getCondition()){
-		// return this->proba + inVisitor.getChildren().at(0)->accept(this);
+		 return this->proba + inVisitor.getChildren().at(0)->accept(*this);
 	}
+	return 0;//to remove compiling warning
 }
 
 double EvalVisitor::visit(Transfert& transVisitor){
 	transVisitor.getParent();
-	//return transVisitor.getLink()->getTop()->accept(this);
+	return transVisitor.getLink()->getTop()->accept(*this);
 
 }
 
 double EvalVisitor::visit(VotingOR& vorVisitor){
 	vorVisitor.getParent();
-	// return vorVisitor.getSubTree()->accept(this);
+	 return vorVisitor.getSubTree()->accept(*this);
 }
 
 double EvalVisitor::visit(Container& eventVisitor){
 	eventVisitor.getParent();
-	// return eventVisitor.getEvent()->getDistribution()->getProbability(time);
+	 return eventVisitor.getEvent()->getDistribution()->getProbability(time);
 }
-*/
