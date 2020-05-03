bf7519dd9ae8bbd50126de972ebcece3520d747b
Andrewraks <43434639+Andrewraks@users.noreply.github.com>
2020-05-03 15:38:30 +0200

    add Evaluator module  (#21)
    * evaluator creation
    
    * Update EvalVisitor.cc
    
    * UPDATE
    
    * Update FTEdit_Evaluator.hh
    
    * Compilation fix + indent
    
    Co-authored-by: Adrien Soursou <asoursou@student.42.fr>

---
+++ src/evaluator/EvalVisitor.cc
@@ -0,0 +1,78 @@
+#include "EvalVisitor.hh"
+
+/***
+ *  ATTENTION NE PAS FAIRE ATTENTION AU CODE "RETURN 0" OU "GETPARENTS" LE VRAI CODE SE TROUVE ACTUELLEMENT ENTRE COMMENTAIRE
+ * EN ATTENTE DE L'IMPLEMENTATION DES METHODES ACCEPT POUR LE EVALVISITOR DANS LE MODULE EDITEUR
+ *
+ *
+ * ***/
+
+using namespace std;
+
+template<typename Base, typename T>
+inline bool instanceof(const T*) {
+	return is_base_of<Base, T>::value;
+}
+
+EvalVisitor::EvalVisitor(double time,double proba){
+	this->time = time;
+	this->proba = proba;
+}
+
+double EvalVisitor::getProba(){
+	return proba;
+}
+
+void EvalVisitor::setTime(double time){
+	this->time = time;
+}
+
+void EvalVisitor::reset(){
+	this->proba = 0;
+}
+/*
+double EvalVisitor::visit(And& andVisitor){
+
+
+	for(int i = 1; i < andVisitor.getChildren().size() ; i++){
+		if(this->proba ==0){
+			//return andVisitor.getChildren().at(i)->accept(this);
+
+		}else{
+		// return this->proba * andVisitor.getChildren().at(i)->accept(this);
+		}
+	}
+}
+
+double EvalVisitor::visit(Or&  orVisitor){
+
+	for(int i = 0; i < orVisitor.getChildren().size() ; i++){
+
+			//return this->proba + orVisitor.getChildren().at(i)->accept(this);
+		}
+
+}
+
+double EvalVisitor::visit(Inhibit& inVisitor){
+	inVisitor.getParent();
+	if(inVisitor.getCondition()){
+		// return this->proba + inVisitor.getChildren().at(0)->accept(this);
+	}
+}
+
+double EvalVisitor::visit(Transfert& transVisitor){
+	transVisitor.getParent();
+	//return transVisitor.getLink()->getTop()->accept(this);
+
+}
+
+double EvalVisitor::visit(VotingOR& vorVisitor){
+	vorVisitor.getParent();
+	// return vorVisitor.getSubTree()->accept(this);
+}
+
+double EvalVisitor::visit(Container& eventVisitor){
+	eventVisitor.getParent();
+	// return eventVisitor.getEvent()->getDistribution()->getProbability(time);
+}
+*/
