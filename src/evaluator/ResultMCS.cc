0b2b7bbf55d769e2550ae86869c762b773f2fed9
Xunyue Hu <xunyue.hu@ens.uvsq.fr>
2020-05-26 22:01:16 +0200

    debug pour top proba de MCS

--- src/evaluator/ResultMCS.cc
+++ src/evaluator/ResultMCS.cc
@@ -96,23 +96,35 @@ ResultMCS::ResultMCS(Gate* top,double missionTime,double step) : Evaluator(top,m
 
         if(i%2==0)/*p=s1-s2+s3-0.5s4*/
         {
-            if(i==klimit-1 && mcs.size()>4)/*quand on a trop de coupes minimales >4, on prend que S1,S2,S3 et moitie de S4*/
+            if(i==klimit-1 && mcs.size()>klimit-1)/*quand on a trop de coupes minimales >4, on prend que S1,S2,S3 et moitie de S4*/
             {                                   /*pour obtenir un resultat approché*/
                 p -= s/2;
             }
-            p -= s;
+            else{
+                p -= s;
+            }
+
         }
         else
         {
-            if(i==klimit-1 && mcs.size()>4)
+            if(i==klimit-1 && mcs.size()>klimit-1)
             {
                 p += s/2;
             }
-            p += s;/*proba de top*/
+            else{
+                 p += s;/*proba de top*/
+            }
+
         }
 
     }
-    probabilities << p;/*probabilities.last() = proba de top*/
+    if(p>1){
+        probabilities << 1;
+    }
+    else{
+        probabilities << p;/*probabilities.last() = proba de top*/
+    }
+
 
 
 
